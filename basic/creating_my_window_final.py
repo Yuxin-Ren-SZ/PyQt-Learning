a538d8f20aec24835c4cc942adc40e7e501f6c21
Yuxin Ren <yxren@ucdavis.edu>
2022-12-10 19:33:33 -0800

    page 17

---
+++ basic/creating_my_window_final.py
@@ -0,0 +1,32 @@
+import sys
+
+from PyQt5.QtCore import (
+    QSize,
+    Qt,
+)
+
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPushButton,
+)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My PyQt5 Window")
+
+        button = QPushButton("Button")
+
+        self.setFixedSize(QSize(400, 300))
+
+        self.setCentralWidget(button)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
