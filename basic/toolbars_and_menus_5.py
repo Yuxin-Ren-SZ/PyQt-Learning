66f627bf9b191c95f46b6c721096d729034391d4
Yuxin Ren <yxren@ucdavis.edu>
2022-12-12 21:09:05 -0800

    page 103

---
+++ basic/toolbars_and_menus_5.py
@@ -0,0 +1,54 @@
+import os
+import sys
+
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (
+    QApplication,
+    QAction,
+    QMainWindow,
+    QLabel,
+    QToolBar,
+    QStatusBar,
+)
+
+
+basdedir = os.path.dirname(__file__)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("PyQt5")
+
+        label = QLabel("Hellow")
+        label.setAlignment(Qt.AlignCenter)
+
+        self.setCentralWidget(label)
+
+        toolbar = QToolBar("Main Toolbar")
+        self.addToolBar(toolbar)
+
+        button_action = QAction(
+            QIcon(os.path.join(basdedir, "image/bug.png")),
+            "Button",
+            self,
+        )
+        button_action.setStatusTip("this is button")
+        button_action.triggered.connect(self.onToolBarClick)
+        button_action.setCheckable(True)
+        toolbar.addAction(button_action)
+
+        self.setStatusBar(QStatusBar(self))
+
+    def onToolBarClick(self, s):
+        print("click", s)
+
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()
