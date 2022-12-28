615da5bf6a90bf4f2f14f69e51fb1f03cbcc31e7
Yuxin Ren <yxren@ucdavis.edu>
2022-12-27 21:50:02 -0800

    page 208
    Also tried change font settings in qtdesign

---
+++ designer/compiled_example2.py
@@ -0,0 +1,31 @@
+import random
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+        QApplication,
+        QMainWindow,
+)
+
+from MainWindow2 import Ui_MainWindow
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.show()
+        
+        self.pushButton.pressed.connect(self.update_label)
+
+    def update_label(self):
+        n = random.randint(1, 6)
+        self.label.setText("%d" % n)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
+
